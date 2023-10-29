c7c398aa6264d47f2a68b4899dc9fbb50fdecf73
Dibwa <kdibwa3@gmail.com>
2023-10-28 20:24:17 -0700

    bbb

--- example/lib/screens/print_page.dart
+++ example/lib/screens/print_page.dart
@@ -527,7 +527,7 @@ class _PrintPageState extends State<PrintPage>
                             ),
                             Expanded(
                               child: DropdownButton(
-                                items: getBluetoots(),
+                                items: _getDeviceItems(),
                                 onChanged: (BluetoothInfo? value) =>
                                     setState(() => items_  = value),
                                 value:items_ ,
